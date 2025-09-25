0879747c7619b694300a3313385692a7c1907af6
Flávio Luiz Bé <104390015+flavio50k@users.noreply.github.com>
2025-09-25 20:24:34 -0300

    Create ex02.js

---
+++ novos-codigos/ex02.js
@@ -0,0 +1,13 @@
+// Declaração de variáveis
+// Atribuição de valores
+// Nomes de variáveis (nome, tipo, xp)
+// Reatribuição de valores (xp = xp + 1500)
+// Tipos de dados (String, String, Number)
+
+let nome = "Gandalf";
+let tipo = "Mago";
+let xp = 1500;
+
+xp = xp + 1500;
+
+console.log("Seu personagem é " + nome + " do tipo " + tipo + " e xp: " + xp);
